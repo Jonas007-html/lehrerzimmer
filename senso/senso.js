091d04b195e10e02bc778d9b07b91b3029ef3372
Jonas007-html <jokummer18@gmail.com>
2024-12-17 21:05:21 +0100

    blinken nach zufall

--- senso/senso.js
+++ senso/senso.js
@@ -1,22 +1,83 @@
-let rot = document.getElementById("rot")
-let rotStatus = 1;
-function setRot(){
-    if(rotStatus == 1){
-        rot.style.background = "red"
-    }else{
-        rot.style.background = "lightgray"
-    }
+/*
+function stateChange(newState) {
+    setTimeout(function () {
+        if (newState == -1) {
+            console.log('VIDEO HAS STOPPED');
+        }
+    }, 5000);
 }
+
+stateChange(-1);
+*/
+function lassSekundenVergehen(){
+    return new Promise((sekundeVergangen) => {
+        setTimeout(() => {
+            sekundeVergangen();
+        }, 1000);
+    })
+}
+
+let rot = document.getElementById("rot")
+
+function setRot(rotStatus){
+    return new Promise((resolve) => {
+        if(rotStatus = 1){
+            rot.style.background = "red"
+        }else{
+            rot.style.background = "lightgray"
+        }
+        setTimeout( function () {
+            if(rotStatus = 1){
+                rot.style.background = "lightgrey"
+            }else{
+                rot.style.background = "red"
+            };
+            resolve();
+        }, 1000);
+        //console.log("rot abgespielt")
+    })    
+}
+
+async function callRot () {
+    return new Promise(async (farbeGesetzt) => {
+        //console.log("anfang")
+        await setRot(1);
+        //console.log("ende es sollt funktioniert haben");
+        farbeGesetzt();
+    })   
+}
+
 
 
 let blau = document.getElementById("blau")
-let blauStatus = 0;
-if(blauStatus == 1){
-    blau.style.background = "blue"
-}else{
-    blau.style.background = "lightgray"
+//let blauStatus = 0;
+
+function setBlau(blauStatus){
+    return new Promise((fertig) => {
+        if(blauStatus = 1){
+            blau.style.background = "blue"
+        }else{
+            blau.style.background = "lightgray"
+        }
+        setTimeout( function () {
+            if(blauStatus = 1){
+                blau.style.background = "lightgrey"
+            }else{
+                blau.style.background = "blue"
+            };
+            fertig();
+        }, 1000);
+        //console.log("rot abgespielt")
+    })    
 }
 
+async function callBlau() {
+    return new Promise(async(blauGesetzt) => {
+        await setBlau(1);
+        blauGesetzt();
+    })
+}
+/*
 let gruen = document.getElementById("gruen")
 let gruenStatus = 0;
 if(gruenStatus == 1){
@@ -27,26 +88,47 @@ if(gruenStatus == 1){
 
 let gelb = document.getElementById("gelb")
 let gelbStatus = 0;
-if(rotStatus == 1){
-    rot.style.background = "red"
+if(gelbStatus == 1){
+    gelb.style.background = "yellow"
 }else{
-    rot.style.background = "lightgray"
+    gelb.style.background = "lightgray"
 }
-
+*/
 let alleFarben = []
 
 function erstelleZufallsFarbe() {
-    let createRandomFarbe = Math.floor(Math.random() * 4) + 1;
+    let createRandomFarbe = Math.floor(Math.random() * 2) + 1;
     alleFarben.push(createRandomFarbe)
 }
 
 erstelleZufallsFarbe();
 erstelleZufallsFarbe();
 erstelleZufallsFarbe();
+erstelleZufallsFarbe();
+erstelleZufallsFarbe();
 
 console.log(alleFarben)
 
-function play() {
-    
+async function farbenDurchlaufen() {
+    for(let i = 0; i < alleFarben.length; i++){
+        if(alleFarben[i] == 1){
+            for(let i = 0; i < 1; i++){
+                await callRot();
+                await lassSekundenVergehen()
+                //console.log("in roter kleinen schleife")
+            }
+        }else if(alleFarben[i] == 2){
+            await callBlau();
+            await lassSekundenVergehen()
+            //console.log("in blauer kleiner schleife")
+        }
+        //erstelleZufallsFarbe();
+        //console.log(alleFarben)
+    }
+
+    console.log("ganz am ende angelangt")
 }
-setRot();
+
+
+farbenDurchlaufen()
+
