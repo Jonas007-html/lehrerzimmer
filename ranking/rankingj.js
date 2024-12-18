7e9dcd3db9fcc54fe1e41f57504cc4ffe5856fd9
nico7-2f <nicolino.volz@gmail.com>
2024-12-18 19:58:14 +0100

    Ranking main page

---
+++ ranking/rankingj.js
@@ -0,0 +1,13 @@
+function displayBestMemoryResult() {
+    const rankings = JSON.parse(localStorage.getItem("rankings")) || [];
+
+    if (rankings.length > 0) {
+        const bestResult = rankings[0];
+        document.getElementById("memory-bestzeit").textContent = `Bestzeit: ${bestResult.score} Sekunden`;
+        document.getElementById("memory-player").textContent = `Von: ${bestResult.name} Klasse: ${bestResult.klasse}`;
+    } else {
+        document.getElementById("memory-bestzeit").textContent = "Bestzeit: -";
+        document.getElementById("memory-player").textContent = "Von: -";
+    }
+}
+window.onload = displayBestMemoryResult;
