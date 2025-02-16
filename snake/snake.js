6848586d98d62d53f158196cd1bdc4827f6eb791
Jonas007-html <jokummer18@gmail.com>
2025-02-16 16:18:56 +0100

    starten, pausieren und neue runde starten; vareiable anzahl an essen

--- snake/snake.js
+++ snake/snake.js
@@ -1,120 +1,135 @@
 let gameField = document.getElementById("gameField");
 
-let zeilen = 20;
-let spalten = 20;
+let zeilen = 15;
+let spalten = 15;
 
-let direction = "down"
+let direction = "down";
+let punkte = 0;
+let innerScore = document.getElementById("scoreDisplay");
 
-function createWorld(){
+let geschwindigkeit = 150;
+let spielAblaufen;
+let spielLaeuft = false;
+
+let touchStartX = 0;
+let touchStartY = 0;
+
+let originalSnake = [{ x: 1, y: 2 }, { x: 1, y: 1 }, { x: 1, y: 0 }]; // wird nicht verändert sondern wieder verwendet wenn eine neue Runde gestartet wird
+let snake = [...originalSnake];
+
+let anzahlFood = 3
+
+function createWorld() {
     let field = "";
-    for(let y = 0; y < zeilen; y++){
-        field += '<div class="row">'
-        for(let x = 0; x < spalten; x++){
-            field += `<div class="field" data-cords="${x}_${y}"></div>`
+    for (let y = 0; y < zeilen; y++) {
+        field += '<div class="row">';
+        for (let x = 0; x < spalten; x++) {
+            field += `<div class="field" data-cords="${x}_${y}"></div>`;
         }
-        field += '</div>'
+        field += '</div>';
     }
     gameField.innerHTML = field;
 }
 createWorld();
 
-window.addEventListener("keydown", function(e){
-    if((e.key == "ArrowDown" || e.key == "s") && direction != "up"){
-        direction = "down"
-    }
-    if((e.key == "ArrowUp" || e.key == "w") && direction != "down"){
-        direction = "up"
-    }
-    if((e.key == "ArrowLeft" || e.key == "a") && direction != "right"){
-        direction = "left"
-    }
-    if((e.key == "ArrowRight" || e.key == "d") && direction != "left"){
-        direction = "right"
-    }
-})
+snake.forEach(function(e) {
+    document.querySelector(`[data-cords="${e.x}_${e.y}"]`).classList.add("snake");
+});
 
-let touchStartX = 0;
-let touchStartY = 0;
-window.addEventListener("touchstart", function(e){
+window.addEventListener("keydown", function(e) {
+    if ((e.key == "ArrowDown" || e.key == "s") && direction != "up") {
+        direction = "down";
+    }
+    if ((e.key == "ArrowUp" || e.key == "w") && direction != "down") {
+        direction = "up";
+    }
+    if ((e.key == "ArrowLeft" || e.key == "a") && direction != "right") {
+        direction = "left";
+    }
+    if ((e.key == "ArrowRight" || e.key == "d") && direction != "left") {
+        direction = "right";
+    }
+});
+
+window.addEventListener("touchstart", function(e) {
     let touch = e.touches[0];
     touchStartX = touch.clientX;
     touchStartY = touch.clientY;
-})
+});
 
-window.addEventListener("touchmove", function(e){
+window.addEventListener("touchmove", function(e) {
     let touch = e.touches[0];
     let x = touch.clientX;
     let y = touch.clientY;
     let dx = x - touchStartX;
     let dy = y - touchStartY;
-    if(Math.abs(dx) > Math.abs(dy)){
-        if(dx > 0 && direction != "left"){
+    if (Math.abs(dx) > Math.abs(dy)) {
+        if (dx > 0 && direction != "left") {
             direction = "right";
-        }else if(dx < 0 && direction != "right"){
+        } else if (dx < 0 && direction != "right") {
             direction = "left";
         }
-    }else{
-        if(dy > 0 && direction != "up"){
+    } else {
+        if (dy > 0 && direction != "up") {
             direction = "down";
-        }else if(dy < 0 && direction != "down"){
+        } else if (dy < 0 && direction != "down") {
             direction = "up";
         }
     }
-})
+});
 
-
-let snake = [{x: 1, y: 2}, {x: 1, y: 1}, {x: 1, y: 0}];
-snake.forEach(function(e){
-    document.querySelector(`[data-cords="${e.x}_${e.y}"]`).classList.add("snake")
-})
-let geschwindigkeit = 150
-let spielAblaufen = setInterval(moveSnake, geschwindigkeit);
-
-function moveSnake(){
+function moveSnake() {
     let kopf = snake[0];
-    let neuerKopf = {x: kopf.x, y: kopf.y};
-    if(direction == "down"){
+    let neuerKopf = { x: kopf.x, y: kopf.y };
+    if (direction == "down") {
         neuerKopf.y++;
-    }else if(direction == "up"){
+    } else if (direction == "up") {
         neuerKopf.y--;
-    }else if(direction == "left"){
+    } else if (direction == "left") {
         neuerKopf.x--;
-    }else if(direction == "right"){
+    } else if (direction == "right") {
         neuerKopf.x++;
     }
-    //console.log("neuerKopf:", neuerKopf);
     if (neuerKopf.x < 0 || neuerKopf.x >= spalten || neuerKopf.y < 0 || neuerKopf.y >= zeilen || checkSelfCollision()) {
         clearInterval(spielAblaufen);
         console.log("Game Over");
+        loose();
     } else {
-        //console.log("bedingung nicht erfüllt");
-        //document.querySelector(`[data-cords="${kopf.x}_${kopf.y}"]`).classList.remove("snake")
-        snake.unshift(neuerKopf);
-        document.querySelector(`[data-cords="${neuerKopf.x}_${neuerKopf.y}"]`).classList.add("snake")
-        if(!checkCollision()){
+        if (!checkCollision()) {
             let schwanz = snake.pop();
-            document.querySelector(`[data-cords="${schwanz.x}_${schwanz.y}"]`).classList.remove("snake")
+            document.querySelector(`[data-cords="${schwanz.x}_${schwanz.y}"]`).classList.remove("snake");
         }
-        
-    }   
+        let tatsächlicheEssensAnzahl = document.querySelectorAll(".food")
+        if(tatsächlicheEssensAnzahl < anzahlFood){ // dient als absicherung falls sehr schnell hintereinander 2 mal gegessen wird damit immer die ursprücngliche anzahl wieer hergestellt wird ( manchmal waren dann nur noch 1 weniger da)
+            generateFood()
+        }
+        snake.unshift(neuerKopf);
+        document.querySelector(`[data-cords="${neuerKopf.x}_${neuerKopf.y}"]`).classList.add("snake");
+    }
 }
 
-function generateFood(){
+function generateFood() {
     let x = Math.floor(Math.random() * spalten);
     let y = Math.floor(Math.random() * zeilen);
-    document.querySelector(`[data-cords="${x}_${y}"]`).classList.add("food")
+    document.querySelector(`[data-cords="${x}_${y}"]`).classList.add("food");
 }
-generateFood();
+for(let i = 0; i < anzahlFood; i++){
+    generateFood();
+}
 
-function checkCollision(){
+function checkCollision() {
     let isCollision = false;
     let kopf = snake[0];
-    let food = document.querySelector(".food");
-    if(kopf.x == food.getAttribute("data-cords").split("_")[0] && kopf.y == food.getAttribute("data-cords").split("_")[1]){
-        console.log("collision");
-        isCollision = true;
-        food.classList.remove("food");
-        generateFood();
+    let food = document.querySelectorAll(".food");
+    for(let i = 0; i < food.length; i++){
+        if (kopf.x == food[i].getAttribute("data-cords").split("_")[0] && kopf.y == food[i].getAttribute("data-cords").split("_")[1]) {
+            console.log("collision");
+            punkte = punkte + 1;
+            innerScore.innerText = punkte + " Punkte";
+            isCollision = true;
+            food[i].classList.remove("food");
+            generateFood();
+        }
     }
     return isCollision;
 }
@@ -127,4 +142,57 @@ function checkSelfCollision() {
         }
     }
     return false;
+}
+
+let startButton = document.getElementById("start");
+
+function startGame() {
+    if (spielLaeuft == false) {
+        spielAblaufen = setInterval(moveSnake, geschwindigkeit);
+        spielLaeuft = true;
+        startButton.innerText = "Spiel pausieren";
+    } else if (spielLaeuft == true) {
+        clearInterval(spielAblaufen);
+        spielLaeuft = false;
+        startButton.innerText = "Spiel fortführen";
+    }
+}
+
+startButton.addEventListener("click", startGame);
+
+function loose() {
+    spielLaeuft = false;
+    direction = "down";
+    startButton.innerText = "Neue Runde starten";
+    startButton.removeEventListener("click", startGame); 
+    startButton.addEventListener("click", () => {
+        allesZuruecksetzten();
+        spielLaeuft = false;
+        startGame()
+        startButton.innerText = "Spiel pausieren";
+        startButton.addEventListener("click", startGame); 
+    }, { once: true }); // Der Listener wird nur einmal ausgelöst
+}
+
+function allesZuruecksetzten() {
+    punkte = 0;
+    innerScore.innerText = punkte + " Punkte";
+
+    let alteSchlange = document.querySelectorAll(".snake");
+    for (let i = 0; i < alteSchlange.length; i++) {
+        alteSchlange[i].classList.remove("snake");
+    }
+
+    let altesEssen = document.querySelectorAll(".food")
+    for(let i = 0; i < altesEssen.length; i++){
+        altesEssen[i].classList.remove("food")
+    }
+    for(let i = 0; i < anzahlFood; i++){
+        generateFood()
+    }
+ 
+    originalSnake.forEach(function(e) {
+        document.querySelector(`[data-cords="${e.x}_${e.y}"]`).classList.add("snake");
+    });
+    snake = [...originalSnake];
 }
